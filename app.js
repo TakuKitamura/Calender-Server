17c033257a390f23e8f0cdc614e2961b68233ecc
Taku Kitamura <tikutaku0128@icloud.com>
2018-03-11 19:11:50 +0900

    微修正

--- app.js
+++ app.js
@@ -1,221 +1,229 @@
 // クライアントからのレスポンスを受け取り、適切なファイルに処理を依頼する
 // 必要なファイルを読み込み
-var http = require('http')
-var https = require('https')
-var url = require('url')
-var fs = require('fs')
-var server = http.createServer()
+var http = require("http");
+var https = require("https");
+var url = require("url");
+var fs = require("fs");
+var server = http.createServer();
 
-var urlParse = require('./node-lib/urlParse')
+var urlParse = require("./node-lib/urlParse");
 
-var apiKey = 'AIzaSyDndUePQ1TWVfNQevNk6GEENmRtmpJUiHM'
-
+var apiKey = "xxx";
 
 // http.createServerがrequestされたら、(イベントハンドラ)
-server.on('request', function (req, res) {
+server.on("request", function(req, res) {
   // Responseオブジェクトを作成し、その中に必要な処理を書いていき、条件によって対応させる
 
   var Response = {
-
-    "renderHTML": function () {
-      var contens = fs.readFile('./public/index.html', 'utf-8', function (err, data) {
-
+    renderHTML: function() {
+      var contens = fs.readFile("./public/index.html", "utf-8", function(
+        err,
+        data
+      ) {
         res.writeHead(200, {
-          'content-Type': 'text/html'
-        })
+          "content-Type": "text/html"
+        });
 
-        res.write(data)
-        res.end()
-
-      })
-
+        res.write(data);
+        res.end();
+      });
     },
 
-    "renderCSS": function () {
-      var contens = fs.readFile('./public/css/map.css', 'utf-8', function (err, data) {
-
+    renderCSS: function() {
+      var contens = fs.readFile("./public/css/map.css", "utf-8", function(
+        err,
+        data
+      ) {
         res.writeHead(200, {
-          'content-Type': 'text/css'
-        })
+          "content-Type": "text/css"
+        });
 
-        res.write(data)
-        res.end()
-
-      })
-
+        res.write(data);
+        res.end();
+      });
     },
 
-    "programStart": function () {
-      var contens = fs.readFile('./public/js/program-start.js', 'utf-8', function (err, data) {
+    programStart: function() {
+      var contens = fs.readFile(
+        "./public/js/program-start.js",
+        "utf-8",
+        function(err, data) {
+          res.writeHead(200, {
+            "content-Type": "text/plain"
+          });
 
-        res.writeHead(200, {
-          'content-Type': 'text/plain'
-        })
-
-        res.write(data)
-        res.end()
-
-      })
-
+          res.write(data);
+          res.end();
+        }
+      );
     },
 
-    "map": function () {
-      var contens = fs.readFile('./public/js/map.js', 'utf-8', function (err, data) {
-
+    map: function() {
+      var contens = fs.readFile("./public/js/map.js", "utf-8", function(
+        err,
+        data
+      ) {
         res.writeHead(200, {
-          'content-Type': 'text/plain'
-        })
+          "content-Type": "text/plain"
+        });
 
-        res.write(data)
-        res.end()
-
-      })
-
+        res.write(data);
+        res.end();
+      });
     },
 
-    "showPlan": function () {
-      var contens = fs.readFile('./public/js/show-plan.js', 'utf-8', function (err, data) {
-
+    showPlan: function() {
+      var contens = fs.readFile("./public/js/show-plan.js", "utf-8", function(
+        err,
+        data
+      ) {
         res.writeHead(200, {
-          'content-Type': 'text/plain'
-        })
+          "content-Type": "text/plain"
+        });
 
-        res.write(data)
-        res.end()
-
-      })
-
+        res.write(data);
+        res.end();
+      });
     },
 
-    "showRoute": function () {
-      var contens = fs.readFile('./public/js/show-route.js', 'utf-8', function (err, data) {
-
+    showRoute: function() {
+      var contens = fs.readFile("./public/js/show-route.js", "utf-8", function(
+        err,
+        data
+      ) {
         res.writeHead(200, {
-          'content-Type': 'text/plain'
-        })
+          "content-Type": "text/plain"
+        });
 
-        res.write(data)
-        res.end()
-
-      })
-
+        res.write(data);
+        res.end();
+      });
     },
 
-    "directionsService": function () {
-      var contens = fs.readFile('./public/js/distance-matrix-service.js', 'utf-8', function (err, data) {
+    directionsService: function() {
+      var contens = fs.readFile(
+        "./public/js/distance-matrix-service.js",
+        "utf-8",
+        function(err, data) {
+          res.writeHead(200, {
+            "content-Type": "text/plain"
+          });
 
-        res.writeHead(200, {
-          'content-Type': 'text/plain'
-        })
-
-        res.write(data)
-        res.end()
-
-      })
-
+          res.write(data);
+          res.end();
+        }
+      );
     },
 
-    "googleApis": function () {
-      var contens = fs.readFile('https://maps.googleapis.com/maps/api/js?key=' + apiKey + '&libraries=places&callback=programStart', 'utf-8', function (err, data) {
+    googleApis: function() {
+      var contens = fs.readFile(
+        "https://maps.googleapis.com/maps/api/js?key=" +
+          apiKey +
+          "&libraries=places&callback=programStart",
+        "utf-8",
+        function(err, data) {
+          res.writeHead(200, {
+            "content-Type": "text/plain"
+          });
 
-        res.writeHead(200, {
-          'content-Type': 'text/plain'
-        })
-
-        res.write(data)
-        res.end()
-
-      })
-
+          res.write(data);
+          res.end();
+        }
+      );
     },
 
-    "apiServer": function (getDirectionsObj) {
+    apiServer: function(getDirectionsObj) {
       // console.log(str)
-      console.log(uri + 'だよ')
+      console.log(uri + "だよ");
 
-      const googleMapsClient = require('./node_modules/@google/maps').createClient({
-        key: apiKey
-      })
+      const googleMapsClient = require("./node_modules/@google/maps").createClient(
+        {
+          key: apiKey
+        }
+      );
 
-      getDirectionsObj['language'] = 'ja'
-      getDirectionsObj['region'] = 'JP'
-      getDirectionsObj['origin'] = getDirectionsObj['origin_lat'] + ', ' + getDirectionsObj['origin_lng']
-      getDirectionsObj['destination'] = getDirectionsObj['destination_lat'] + ', ' + getDirectionsObj['destination_lng']
+      getDirectionsObj["language"] = "ja";
+      getDirectionsObj["region"] = "JP";
+      getDirectionsObj["origin"] =
+        getDirectionsObj["origin_lat"] + ", " + getDirectionsObj["origin_lng"];
+      getDirectionsObj["destination"] =
+        getDirectionsObj["destination_lat"] +
+        ", " +
+        getDirectionsObj["destination_lng"];
 
-      if( 'arrival_time' in getDirectionsObj ) {
-        getDirectionsObj['arrival_time'] = new Date(getDirectionsObj['arrival_time'] + ' UTC')
+      if ("arrival_time" in getDirectionsObj) {
+        getDirectionsObj["arrival_time"] = new Date(
+          getDirectionsObj["arrival_time"] + " UTC"
+        );
       }
 
-      if( 'departure_time' in getDirectionsObj) {
-        getDirectionsObj['departure_time'] = new Date(getDirectionsObj['departure_time'] + ' UTC')
+      if ("departure_time" in getDirectionsObj) {
+        getDirectionsObj["departure_time"] = new Date(
+          getDirectionsObj["departure_time"] + " UTC"
+        );
       }
 
-      if( getDirectionsObj['mode'] === 'transit' ) {
-        getDirectionsObj['transit_mode'] = 'bus'
+      if (getDirectionsObj["mode"] === "transit") {
+        getDirectionsObj["transit_mode"] = "bus";
       }
 
       // delete getDirectionsObj['mode']
       // delete getDirectionsObj['arrival_time']
-      delete getDirectionsObj['origin_lat']
-      delete getDirectionsObj['origin_lng']
-      delete getDirectionsObj['destination_lat']
-      delete getDirectionsObj['destination_lng']
+      delete getDirectionsObj["origin_lat"];
+      delete getDirectionsObj["origin_lng"];
+      delete getDirectionsObj["destination_lat"];
+      delete getDirectionsObj["destination_lng"];
 
-      console.log(getDirectionsObj)
+      console.log(getDirectionsObj);
       // console.log(googleMapsClient.directions)
 
-      googleMapsClient.directions( getDirectionsObj,
-        function(err,response) {
-          // console.log('HELLO')
-          console.log(err)
-          console.log(response)
-          if(!err) {
-            console.log(response)
-            writeJson(response)
-          }
+      googleMapsClient.directions(getDirectionsObj, function(err, response) {
+        // console.log('HELLO')
+        console.log(err);
+        console.log(response);
+        if (!err) {
+          console.log(response);
+          writeJson(response);
         }
-      )
+      });
 
       function writeJson(response) {
-        console.log(response)
+        console.log(response);
 
         function update(json) {
           res.writeHead(200, {
-            'content-Type': 'text/json; charset=utf-8'
-          })
-          res.write(JSON.stringify(json))
+            "content-Type": "text/json; charset=utf-8"
+          });
+          res.write(JSON.stringify(json));
 
-          res.end()
+          res.end();
         }
 
-        var url = response.requestUrl
-        https.get(url, function(res){
-          var body = ''
-          res.setEncoding('utf8')
+        var url = response.requestUrl;
+        https
+          .get(url, function(res) {
+            var body = "";
+            res.setEncoding("utf8");
 
-          res.on('data', function(chunk){
-            body += chunk
+            res.on("data", function(chunk) {
+              body += chunk;
+            });
+
+            res.on("end", function(res) {
+              ret = JSON.parse(body);
+              // console.log(ret)
+              update(ret);
+            });
           })
-
-          res.on('end', function(res){
-            ret = JSON.parse(body)
-            // console.log(ret)
-            update(ret)
-          })
-        }).on('error', function(e){
-          console.log(e.message) //エラー時
-        })
-
-
+          .on("error", function(e) {
+            console.log(e.message); //エラー時
+          });
       }
-
     }
-
-
-  }
+  };
 
   // urlのpathをuriに代入
-  var uri = url.parse(req.url).pathname
+  var uri = url.parse(req.url).pathname;
   // console.log(uri)
 
   /*
@@ -263,21 +271,16 @@ server.on('request', function (req, res) {
 
   // APIアクセスのURLチェック にテストケース
   if (/^\/api\/v1\//.test(req.url)) {
+    const getDirectionsObj = urlParse.parseGetRequest(req.url);
 
-    const getDirectionsObj = urlParse.parseGetRequest(req.url)
-
-    if( getDirectionsObj !== 'BADPARAMS' ) {
-      Response["apiServer"](getDirectionsObj)
-      return
+    if (getDirectionsObj !== "BADPARAMS") {
+      Response["apiServer"](getDirectionsObj);
+      return;
+    } else {
+      return;
     }
-
-    else {
-      return
-    }
-
   }
+});
 
-})
-
-server.listen(3000)
-console.log('Server running at http://localhost:3000/')
+server.listen(3000);
+console.log("Server running at http://localhost:3000/");
